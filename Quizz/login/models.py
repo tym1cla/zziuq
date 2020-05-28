6199487eddc3ab5ad04ddb20be8dc93de17daae6
gszmigulan <44379945+gszmigulan@users.noreply.github.com>
2020-05-28 19:45:36 +0200

    dodaję do bazy danych tablice pytań

--- Quizz/login/models.py
+++ Quizz/login/models.py
@@ -22,3 +22,55 @@ class User(models.Model):
         ordering = ["-c_time"]
         verbose_name = "użytkownik"
         verbose_name_plural = "użytkownik"
+
+
+class Biology(models.Model):
+    question=models.CharField(max_length=1000)
+    a_1=models.CharField(max_length=256)
+    a_2 = models.CharField(max_length=256)
+    a_3 = models.CharField(max_length=256)
+    a_4 = models.CharField(max_length=256)
+    correct= models.CharField(max_length=8)
+
+class Entertainment(models.Model):
+    question=models.CharField(max_length=1000)
+    a_1=models.CharField(max_length=256)
+    a_2 = models.CharField(max_length=256)
+    a_3 = models.CharField(max_length=256)
+    a_4 = models.CharField(max_length=256)
+    correct= models.CharField(max_length=8)
+
+class History(models.Model):
+    question=models.CharField(max_length=1000)
+    a_1=models.CharField(max_length=256)
+    a_2 = models.CharField(max_length=256)
+    a_3 = models.CharField(max_length=256)
+    a_4 = models.CharField(max_length=256)
+    correct= models.CharField(max_length=8)
+
+class Literature(models.Model):
+    question=models.CharField(max_length=1000)
+    a_1=models.CharField(max_length=256)
+    a_2 = models.CharField(max_length=256)
+    a_3 = models.CharField(max_length=256)
+    a_4 = models.CharField(max_length=256)
+    correct= models.CharField(max_length=8)
+
+class Science(models.Model):
+    question=models.CharField(max_length=1000)
+    a_1=models.CharField(max_length=256)
+    a_2 = models.CharField(max_length=256)
+    a_3 = models.CharField(max_length=256)
+    a_4 = models.CharField(max_length=256)
+    correct= models.CharField(max_length=8)
+
+class Sport(models.Model):
+    question=models.CharField(max_length=1000)
+    a_1=models.CharField(max_length=256)
+    a_2 = models.CharField(max_length=256)
+    a_3 = models.CharField(max_length=256)
+    a_4 = models.CharField(max_length=256)
+    correct= models.CharField(max_length=8)
+
+ 
+        
