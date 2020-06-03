9b10f5048ecb0698fc9a6feb37972b1512c0c6d1
fkula <f.kulasza98@gmail.com>
2020-06-03 14:55:44 +0200

    Result table design has been added. Main menu design has been improved.

--- Quizz/login/models.py
+++ Quizz/login/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 from category.models import Geography, Entertainment, History, Literature, Science, Sport
-import operator
 
 gender = (
     ('mężczyzna', "♂"),
