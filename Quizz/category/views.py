c290db1612c580f3d06136b20cd079a66bf386f4
gszmigulan <44379945+gszmigulan@users.noreply.github.com>
2020-06-05 13:38:27 +0200

    Update views.py

--- Quizz/category/views.py
+++ Quizz/category/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import QuestionForm
 from .models import *
@@ -17,33 +17,37 @@ def quiz_geography(request):
     title = 'Geography'
     # get user
     current_user = Profile.objects.get(user=request.user)
-    # get user available questions
-    user_questions_list = current_user.geography.all()
-    # get all not used questions
-    questions_list = Geography.objects.all().exclude(id__in=user_questions_list)
-    available_questions_number = len(questions_list)
 
-    if len(questions_list) > 0:
-        # get random question
-        index = random.randrange(len(questions_list))
-        question = questions_list[index]
-    else:
-        question = ''
+    if request.method == 'GET':
+        # get user available questions
+        user_questions_list = current_user.geography.all()
+        # get all not used questions
+        questions_list = Geography.objects.all().exclude(id__in=user_questions_list)
+        available_questions_number = len(questions_list)
 
-    if request.method == 'POST':
-        answer = request.POST.get('answer')
-        if isinstance(question, str):
-            current_user.geography.add(question)
-            pass
+        if len(questions_list) > 0:
+            # get random question
+            index = random.randrange(len(questions_list))
+            question = questions_list[index]
         else:
-            # collect score
-            if int(answer) == question.correct_answer:
-                current_user.score += 1
-
-            current_user.geography.add(question)
-            current_user.save()
+            question = None
 
         return render(request, 'quiz.html', locals())
+
+    if request.method == 'POST':
+        question_id = request.POST.get('id')
+        if len(question_id) > 0:
+            question = Geography.objects.get(id=int(question_id))
+            answer = request.POST.get('answer')
+            if question is not None:
+                # collect score
+                if int(answer) == question.correct_answer:
+                    current_user.score += 1
+
+                current_user.geography.add(question)
+                current_user.save()
+            return redirect(quiz_geography)
+
     return render(request, 'quiz.html', locals())
 
 
@@ -52,32 +56,37 @@ def quiz_entertainment(request):
     title = 'Entertainment'
     # get user
     current_user = Profile.objects.get(user=request.user)
-    # get user available questions
-    user_questions_list = current_user.entertainment.all()
-    # get all not used questions
-    questions_list = Entertainment.objects.all().exclude(id__in=user_questions_list)
-    available_questions_number = len(questions_list)
 
-    if len(questions_list) > 0:
-        # get random question
-        index = random.randrange(len(questions_list))
-        question = questions_list[index]
-    else:
-        question = ''
+    if request.method == 'GET':
+        # get user available questions
+        user_questions_list = current_user.entertainment.all()
+        # get all not used questions
+        questions_list = Entertainment.objects.all().exclude(id__in=user_questions_list)
+        available_questions_number = len(questions_list)
 
-    if request.method == 'POST':
-        answer = request.POST.get('answer')
-        if isinstance(question, str):
-            pass
+        if len(questions_list) > 0:
+            # get random question
+            index = random.randrange(len(questions_list))
+            question = questions_list[index]
         else:
-            # collect score
-            if int(answer) == question.correct_answer:
-                current_user.score += 1
-
-            current_user.entertainment.add(question)
-            current_user.save()
+            question = None
 
         return render(request, 'quiz.html', locals())
+
+    if request.method == 'POST':
+        question_id = request.POST.get('id')
+        if len(question_id) > 0:
+            question = Entertainment.objects.get(id=int(question_id))
+            answer = request.POST.get('answer')
+            if question is not None:
+                # collect score
+                if int(answer) == question.correct_answer:
+                    current_user.score += 1
+
+                current_user.entertainment.add(question)
+                current_user.save()
+            return redirect(quiz_entertainment)
+
     return render(request, 'quiz.html', locals())
 
 
@@ -86,32 +95,37 @@ def quiz_history(request):
     title = 'History'
     # get user
     current_user = Profile.objects.get(user=request.user)
-    # get user available questions
-    user_questions_list = current_user.history.all()
-    # get all not used questions
-    questions_list = History.objects.all().exclude(id__in=user_questions_list)
-    available_questions_number = len(questions_list)
 
-    if len(questions_list) > 0:
-        # get random question
-        index = random.randrange(len(questions_list))
-        question = questions_list[index]
-    else:
-        question = ''
+    if request.method == 'GET':
+        # get user available questions
+        user_questions_list = current_user.history.all()
+        # get all not used questions
+        questions_list = History.objects.all().exclude(id__in=user_questions_list)
+        available_questions_number = len(questions_list)
 
-    if request.method == 'POST':
-        answer = request.POST.get('answer')
-        if isinstance(question, str):
-            pass
+        if len(questions_list) > 0:
+            # get random question
+            index = random.randrange(len(questions_list))
+            question = questions_list[index]
         else:
-            # collect score
-            if int(answer) == question.correct_answer:
-                current_user.score += 1
-
-            current_user.history.add(question)
-            current_user.save()
+            question = None
 
         return render(request, 'quiz.html', locals())
+
+    if request.method == 'POST':
+        question_id = request.POST.get('id')
+        if len(question_id) > 0:
+            question = History.objects.get(id=int(question_id))
+            answer = request.POST.get('answer')
+            if question is not None:
+                # collect score
+                if int(answer) == question.correct_answer:
+                    current_user.score += 1
+
+                current_user.history.add(question)
+                current_user.save()
+            return redirect(quiz_history)
+
     return render(request, 'quiz.html', locals())
 
 
@@ -120,32 +134,37 @@ def quiz_literature(request):
     title = 'Literature'
     # get user
     current_user = Profile.objects.get(user=request.user)
-    # get user available questions
-    user_questions_list = current_user.literature.all()
-    # get all not used questions
-    questions_list = Literature.objects.all().exclude(id__in=user_questions_list)
-    available_questions_number = len(questions_list)
 
-    if len(questions_list) > 0:
-        # get random question
-        index = random.randrange(len(questions_list))
-        question = questions_list[index]
-    else:
-        question = ''
+    if request.method == 'GET':
+        # get user available questions
+        user_questions_list = current_user.literature.all()
+        # get all not used questions
+        questions_list = Literature.objects.all().exclude(id__in=user_questions_list)
+        available_questions_number = len(questions_list)
 
-    if request.method == 'POST':
-        answer = request.POST.get('answer')
-        if isinstance(question, str):
-            pass
+        if len(questions_list) > 0:
+            # get random question
+            index = random.randrange(len(questions_list))
+            question = questions_list[index]
         else:
-            # collect score
-            if int(answer) == question.correct_answer:
-                current_user.score += 1
-
-            current_user.literature.add(question)
-            current_user.save()
+            question = None
 
         return render(request, 'quiz.html', locals())
+
+    if request.method == 'POST':
+        question_id = request.POST.get('id')
+        if len(question_id) > 0:
+            question = Literature.objects.get(id=int(question_id))
+            answer = request.POST.get('answer')
+            if question is not None:
+                # collect score
+                if int(answer) == question.correct_answer:
+                    current_user.score += 1
+
+                current_user.literature.add(question)
+                current_user.save()
+            return redirect(quiz_literature)
+
     return render(request, 'quiz.html', locals())
 
 
@@ -154,32 +173,37 @@ def quiz_science(request):
     title = 'Science'
     # get user
     current_user = Profile.objects.get(user=request.user)
-    # get user available questions
-    user_questions_list = current_user.science.all()
-    # get all not used questions
-    questions_list = Science.objects.all().exclude(id__in=user_questions_list)
-    available_questions_number = len(questions_list)
 
-    if len(questions_list) > 0:
-        # get random question
-        index = random.randrange(len(questions_list))
-        question = questions_list[index]
-    else:
-        question = ''
+    if request.method == 'GET':
+        # get user available questions
+        user_questions_list = current_user.science.all()
+        # get all not used questions
+        questions_list = Science.objects.all().exclude(id__in=user_questions_list)
+        available_questions_number = len(questions_list)
 
-    if request.method == 'POST':
-        answer = request.POST.get('answer')
-        if isinstance(question, str):
-            pass
+        if len(questions_list) > 0:
+            # get random question
+            index = random.randrange(len(questions_list))
+            question = questions_list[index]
         else:
-            # collect score
-            if int(answer) == question.correct_answer:
-                current_user.score += 1
-
-            current_user.science.add(question)
-            current_user.save()
+            question = None
 
         return render(request, 'quiz.html', locals())
+
+    if request.method == 'POST':
+        question_id = request.POST.get('id')
+        if len(question_id) > 0:
+            question = Science.objects.get(id=int(question_id))
+            answer = request.POST.get('answer')
+            if question is not None:
+                # collect score
+                if int(answer) == question.correct_answer:
+                    current_user.score += 1
+
+                current_user.science.add(question)
+                current_user.save()
+            return redirect(quiz_science)
+
     return render(request, 'quiz.html', locals())
 
 
@@ -188,32 +212,37 @@ def quiz_sport(request):
     title = 'Sport'
     # get user
     current_user = Profile.objects.get(user=request.user)
-    # get user available questions
-    user_questions_list = current_user.sport.all()
-    # get all not used questions
-    questions_list = Sport.objects.all().exclude(id__in=user_questions_list)
-    available_questions_number = len(questions_list)
 
-    if len(questions_list) > 0:
-        # get random question
-        index = random.randrange(len(questions_list))
-        question = questions_list[index]
-    else:
-        question = ''
+    if request.method == 'GET':
+        # get user available questions
+        user_questions_list = current_user.sport.all()
+        # get all not used questions
+        questions_list = Sport.objects.all().exclude(id__in=user_questions_list)
+        available_questions_number = len(questions_list)
 
-    if request.method == 'POST':
-        answer = request.POST.get('answer')
-        if isinstance(question, str):
-            pass
+        if len(questions_list) > 0:
+            # get random question
+            index = random.randrange(len(questions_list))
+            question = questions_list[index]
         else:
-            # collect score
-            if int(answer) == question.correct_answer:
-                current_user.score += 1
-
-            current_user.sport.add(question)
-            current_user.save()
+            question = None
 
         return render(request, 'quiz.html', locals())
+
+    if request.method == 'POST':
+        question_id = request.POST.get('id')
+        if len(question_id) > 0:
+            question = Sport.objects.get(id=int(question_id))
+            answer = request.POST.get('answer')
+            if question is not None:
+                # collect score
+                if int(answer) == question.correct_answer:
+                    current_user.score += 1
+
+                current_user.sport.add(question)
+                current_user.save()
+            return redirect(quiz_sport)
+
     return render(request, 'quiz.html', locals())
 
 
